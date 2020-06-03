9a2880122a11ea875aa9fd909c49b7ee9c157d42
walter lima <nitlawalter69@gmail.com>
2020-06-03 12:07:17 -0300

    criando instanciacao de dados

--- src/main/java/com/walter/curso/CursoApplication.java
+++ src/main/java/com/walter/curso/CursoApplication.java
@@ -1,13 +1,34 @@
 package com.walter.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.walter.curso.domain.Categoria;
+import com.walter.curso.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoApplication {
+public class CursoApplication implements CommandLineRunner {
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+
+	
+	
 }
