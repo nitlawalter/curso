fd100a183d2b4f4aac0acbe951b2fc2602686086
walter lima <nitlawalter69@gmail.com>
2020-06-03 03:00:56 -0300

    implementação categoriaService

---
+++ src/main/java/com/walter/curso/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.walter.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.walter.curso.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+	
+
+}
--- src/main/java/com/walter/curso/resources/CategoriaResource.java
+++ src/main/java/com/walter/curso/resources/CategoriaResource.java
@@ -3,26 +3,27 @@ package com.walter.curso.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.walter.curso.domain.Categoria;
+import com.walter.curso.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria c1 = new Categoria(1, "Informatica");
-		Categoria c2 = new Categoria(2, "Escritorio");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(c1);
-		lista.add(c2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);		
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
