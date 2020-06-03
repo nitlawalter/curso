f55aead5d78f53191f6b5ccee5f3cfe5c41ff24b
walter lima <nitlawalter69@gmail.com>
2020-06-03 18:51:50 -0300

    estado e cidade

--- src/main/java/com/walter/curso/CursoApplication.java
+++ src/main/java/com/walter/curso/CursoApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.walter.curso.domain.Categoria;
+import com.walter.curso.domain.Cidade;
+import com.walter.curso.domain.Estado;
 import com.walter.curso.domain.Produto;
 import com.walter.curso.repositories.CategoriaRepository;
+import com.walter.curso.repositories.CidadeRepository;
+import com.walter.curso.repositories.EstadoRepository;
 import com.walter.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,6 +25,12 @@ public class CursoApplication implements CommandLineRunner {
 	@Autowired
 	ProdutoRepository produtoRepository;
 
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
 	}
@@ -41,9 +51,22 @@ public class CursoApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "minas gerais");
+		Estado est2 = new Estado(null, "sao paulo");
+		
+		Cidade cid1 = new Cidade(null, "uberlandia", est1);
+		Cidade cid2 = new Cidade(null, "sao paulo", est2);
+		Cidade cid3 = new Cidade(null, "campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		est2.getCidades().addAll(Arrays.asList(cid2, cid3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
+		
 		
 	}
 
---
+++ src/main/java/com/walter/curso/repositories/CidadeRepository.java
@@ -0,0 +1,13 @@
+package com.walter.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.walter.curso.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+	
+	
+
+}
